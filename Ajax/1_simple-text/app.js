88e5afcc3b9e3a1d7a19979eaeabdf01f16768c0
chen945 <207410076@gms.tku.edu.tw>
2022-05-18 19:06:53 +0800

    w13-p1: use xhr object to get sample.txt, and show it on webpage

---
+++ Ajax/1_simple-text/app.js
@@ -0,0 +1,22 @@
+//從sample.txt 撈資料
+const xhr = new XMLHttpRequest();
+console.log("xhr0", xhr);
+
+xhr.open("GET", "./api/sample.txt");
+xhr.onreadystatechange = function () {
+    console.log("xhr", xhr);
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        // console.log("done");
+        const text = document.createElement("p");
+        text.textContent = xhr.responseText;
+        document.body.appendChild(text);
+    } else {
+        console.log({
+            status: xhr.status,
+            text: xhr.statusText,
+        });
+    }
+};
+
+xhr.send("");
+console.log("hello");
