31893b4269e85b9d3cb782deed48966df3e74a77
chen945 <207410076@gms.tku.edu.tw>
2022-05-25 20:52:40 +0800

    w14-p3: fetch drinks, get null, show no drinks matched

---
+++ cocktaildb_76/src/displayDrinks_76.js
@@ -0,0 +1,17 @@
+import get from "./getElement.js";
+
+const displayDrinks = async ({ drinks }) => {
+    console.log("drinks", drinks);
+    const title = get(".title");
+    const section = get(".section-center");
+    console.log("title", title);
+    console.log("section", section);
+    if (!drinks) {
+        title.textContent = "Sorry, no drinks matched your search";
+        section.innerHTML = null;
+        return;
+    }
+    return section;
+};
+
+export default displayDrinks;
--- cocktaildb_76/src/presentDrinks_76.js
+++ cocktaildb_76/src/presentDrinks_76.js
@@ -1,9 +1,10 @@
 import fetchDrinks from "./fetchDrinks_76.js";
-
+import displayDrinks from "./displayDrinks_76.js";
 const presentDrinks = async (url) => {
     //fetch drinks
     const data = await fetchDrinks(url);
     console.log("drinks data", data);
+    const section = await displayDrinks(data);
 };
 
 export default presentDrinks;
