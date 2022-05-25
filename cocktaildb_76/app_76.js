f0d9d070a71d949d1759a773e16214218d404717
chen945 <207410076@gms.tku.edu.tw>
2022-05-25 21:11:12 +0800

    w14-p4: fetch drinks from given url, and display all drinks

--- cocktaildb_76/app_76.js
+++ cocktaildb_76/app_76.js
@@ -1,6 +1,6 @@
 import presentDrinks from "./src/presentDrinks_76.js";
 
-const URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=doris";
+const URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=d";
 
 window.addEventListener("DOMContentLoaded", () => {
     presentDrinks(URL);
--- cocktaildb_76/src/displayDrinks_76.js
+++ cocktaildb_76/src/displayDrinks_76.js
@@ -11,6 +11,25 @@ const displayDrinks = async ({ drinks }) => {
         section.innerHTML = null;
         return;
     }
+    const newDrinks = drinks
+        .map((drink) => {
+            const {
+                isDrinks: id,
+                strDrink: name,
+                strDrinkThumb: image,
+            } = drink;
+            return `
+        <a href="drink_76.html">
+                <article class="cocktail" data-id="${id}">
+                    <img src="${image}" alt="cocktail" />
+                    <h3>${name}</h3>
+                </article>
+            </a>
+        `;
+        })
+        .join("");
+    title.textContent = "";
+    section.innerHTML = newDrinks;
     return section;
 };
 
