6137b6125210229015ac2c8789bf6b9ceedf10d3
chen945 <207410076@gms.tku.edu.tw>
2022-06-21 15:51:58 +0800

    w17-p2: fetch a local json data with local images

--- w17demo/cocktaildb_76_local_json/api/search/config_76.js
+++ w17demo/cocktaildb_76_local_json/api/search/config_76.js
@@ -1,5 +1,5 @@
-export const searchItems = ["h", "d", "y"];
+export const searchItems = ["h", "d", "orange"];
 
 export const hasNetwork = false;
 
-export const LOCAL_URL = "/api/search/h.json";
+export const LOCAL_URL = "/api/search/orange.json";
